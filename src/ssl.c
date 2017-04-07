20fa6a641cd638566482ecc8012d626bbe1c11cc
Takashi Kojo <kojo@wolfsssl.com>
2017-04-07 11:39:08 +0900

    set pkey->rsa in d2i_PUBKEY

--- src/ssl.c
+++ src/ssl.c
@@ -6047,6 +6047,7 @@ WOLFSSL_EVP_PKEY* wolfSSL_d2i_PUBKEY(WOLFSSL_EVP_PKEY** out, unsigned char** in,
                 pkey->type = EVP_PKEY_RSA;
                 if (out != NULL) {
                     *out = pkey;
+                    pkey->rsa = (WOLFSSL_RSA*)pkey->pkey.ptr;
                 }
 
                 pkey->ownRsa = 1;
